fcb27d16d0db23a7a08d6af3dce676d224dee2dd
Trovaum <ricardopouza@gmail.com>
2022-01-02 23:04:02 -0300

    private set na classe

--- src/main/digitalonebank/Pessoa.kt
+++ src/main/digitalonebank/Pessoa.kt
@@ -1,10 +1,13 @@
 package main.digitalonebank
 
+
 class Pessoa {
+
     var nome: String = "Ricardo"
     var cpf: String = "299.470.628.80"
-}
 
+    private set
+}
 fun main(){
     val dados = Pessoa()
 
