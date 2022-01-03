b7bcd8dcc930d2b803160045c56c23ddd9601bbd
Trovaum <ricardopouza@gmail.com>
2022-01-02 23:19:48 -0300

    inclusão da classe Conta

---
+++ src/main/digitalonebank/Conta.kt
@@ -0,0 +1,17 @@
+package main.digitalonebank
+
+import java.math.BigDecimal
+
+class Conta(
+    val numero: String,
+    val agencia: String,
+    val saldo: BigDecimal
+) {
+    fun deposito(valor: BigDecimal) {
+
+    }
+
+    fun saque(valor: BigDecimal) {
+
+    }
+}
